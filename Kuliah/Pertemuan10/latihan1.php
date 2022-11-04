89f53913fcc25550ca363ac058d32a32bf4b764d
msaidhusni <msaidhusni1208@gmail.com>
2022-11-04 22:26:42 +0700

    menambahkan file

---
+++ Kuliah/Pertemuan10/latihan1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Mahasiswa</title>
+</head>
+<body>
+    
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>#</th>
+            <th>gambar</th>
+            <th>NRP</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+            <th>Aksi</th>
+        </tr>
+
+        <tr>
+            <td>1</td>
+            <td><img src="/Kuliah/img/naruto.jpg" width="60"></td>
+            <td>093839283</td>
+            <td>M Said Husni</td>
+            <td>msaidhusni@yahoo.co.id</td>
+            <td>Teknik Informatika</td>
+            <td>
+                <a href="">Ubah</a> / <a href="">Hapus</a>
+            </td>
+       
+        </tr>
+
+    </table>
+
+</body>
+</html>
